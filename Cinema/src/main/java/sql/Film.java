1d5b9a62b4c121adcbf60afcee67ea9a32e8c80d
unknown <tanguy.vienot.pers@gmail.com>
2022-03-31 17:17:34 +0200

    frames package

--- Cinema/src/main/java/sql/Film.java
+++ Cinema/src/main/java/sql/Film.java
@@ -4,10 +4,45 @@
  */
 package sql;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author Tanguy
  */
-public class Film {
-    
+public class Film extends SQLqry{
+    public Film()
+    {
+        super();
+    }
+    public void insert(String name, String genre, int duration, String imagepath)
+    {
+        try
+        {
+            Class.forName(driver);
+            
+            Connection con = DriverManager.getConnection(url, username, password);
+            String qry = "INSERT INTO film VALUES (UUID(),?,?,?,?)";
+            PreparedStatement st = con.prepareStatement(qry);
+            st.setString(1,name);
+            st.setString(2,genre);
+            st.setInt(3,duration);
+            st.setString(4,imagepath);
+                  
+            
+            st.executeUpdate();        
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 }
